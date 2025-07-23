8858945658ae67ddf83ea669a9bde69935ea845d
Abanoub-Abd-Elmessih <abanoubabdelmessih110@gmail.com>
2025-07-23 19:31:42 +0300

    fix slider

--- js/slider.js
+++ js/slider.js
@@ -1,46 +1,37 @@
-
 // slider
 
-var img = document.getElementById("heroimg");
-var myimg = ["assets/fashionable-woman-brown-coat-beige-hat-posing.jpg", "assets/stylish-woman-leather-coat-black-hat-demonstrate-winter-fashion-trends-white.jpg", "assets/portrait-handsome-confident-hipster-lumbersexual-businessman-model-wearing-casual-white-sweater-trousers.jpg"]
-var i = 0;
+var slides = document.querySelectorAll(".heroImg");
+var current = 0;
 
+function showSlide(index) {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+}
 
 function next() {
-    i++
-    if (i >= myimg.length) { i = 0 }
-    img.src = myimg[i]
-    img.animate([
-        { opacity: 0 },
-        { opacity: 1 }
-    ], {
-        duration: 2000,
-    });
+  current++;
+  if (current >= slides.length) {
+    current = 0;
+  }
+  showSlide(current);
 }
 
 function prev() {
-    i--
-    if (i < 0) { i = myimg.length - 1 } else if (i >= myimg.length) { i - 1 }
-    img.src = myimg[i]
-    img.animate([
-        { opacity: 0 },
-        { opacity: 1 }
-    ], {
-        duration: 3000,
-    });
+  current--;
+  if (current < 0) {
+    current = slides.length - 1;
+  }
+  showSlide(current);
 }
 
-
-window.onload = function () {
-    setInterval(next, 6000);
-};
+setInterval(next, 5000);
 
 // slider
 
 // shop now button
 
 document.querySelector(".herobtn").addEventListener("click", function () {
-    open("sub-pages/shop.html", "_self");
+  open("sub-pages/shop.html", "_self");
 });
 
 // shop now button
@@ -53,13 +44,13 @@ var cart = document.querySelectorAll(".cart");
 var maincart = document.getElementById("maincart");
 
 cart.forEach((item) => {
-    item.addEventListener("click", () => {
-        cartCount++;
-        console.log(cartCount);
-        cartCounter.innerText = cartCount;
-        cartCounter.style.backgroundColor = "green";
-        cart.style.color = "white";
-    });
+  item.addEventListener("click", () => {
+    cartCount++;
+    console.log(cartCount);
+    cartCounter.innerText = cartCount;
+    cartCounter.style.backgroundColor = "green";
+    cart.style.color = "white";
+  });
 });
 
 // cart counter
