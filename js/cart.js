4d0a6abaf70128395880da81a2917e9fc27ca77b
Ahmed Abdo <abdelmottale3@gmail.com>
2025-07-24 02:23:28 +0300

    fix cart

--- js/cart.js
+++ js/cart.js
@@ -3,6 +3,7 @@ var cartTableBody = document.getElementById("cartTableBody");
 
 if (localStorageCart.length == 0) {
   alert("your cart is empty please add some products");
+  open("shop.html", "_self");
 }
 
 if (localStorageCart.length > 0) {
@@ -16,13 +17,54 @@ function createTr(product) {
   var tr = document.createElement("tr");
   tr.innerHTML = `
     <td>
-    <a href="#"><i class="fa-regular fa-circle-xmark"></i></a>
+      <div class="delete-icon">Delete</div>
     </td>
     <td><img src="${product.image}" alt="${product.title}" /></td>
-        <td>${product.title}</td>
-            <td>${product.price}</td>
-            <td><input type="number" value="1" /></td>
-            <td>${product.price}</td>
-          `;
+    <td>${product.title}</td>
+    <td>${product.price}</td>
+    <td><input type="number" value="1" id="counter" /></td>
+    <td id="total">${product.price}</td>
+  `;
+
+  tr.querySelector("#counter").addEventListener("change", function () {
+    var quantity = parseInt(this.value);
+
+    if (isNaN(quantity) || quantity <= 0) {
+      this.value = 1;
+      quantity = 1;
+    }
+
+    var totalPriceCell = tr.querySelector("#total");
+    totalPriceCell.innerHTML = (product.price * quantity);
+
+    var totalValue = document.querySelector("#totalvalue");
+    totalValue.innerHTML = totalPriceCell.innerHTML;
+  })
+
+  var discount = document.querySelector("#discount");
+  var copun = document.querySelector("#copoun");
+
+
+  // discount
+
+  copun.addEventListener("input", function () {
+    if (this.value === "DISCOUNT10") {
+      discount.innerHTML = "10% off";
+      var afterdiscount = document.querySelector("#afterdiscount");
+      var totalValue = document.querySelector("#totalvalue");
+      afterdiscount.innerHTML = Math.ceil(totalValue.innerHTML * 0.9);
+    } else {
+      discount.innerHTML = "Invalid coupon";
+    }
+
+  })
+
+
+  // delete row 
+
+  tr.querySelector('.delete-icon').addEventListener('click', function () {
+    tr.remove();
+  })
+
   return tr;
 }
